80986d5d83c6a6729c0c2993090bfae9d0af0dfa
Automatik02 <pro.descombes@gmail.com>
2023-03-29 00:43:42 -0700

    Add betty style to header file

--- main.h
+++ main.h
@@ -6,13 +6,19 @@
 
 int _strlen(char *count);
 int print_c(va_list c);
-int print_percent (va_list a);
+int print_percent(va_list a);
 int print_s(va_list s);
 int  check_type(char c, va_list list);
 int _printf(const char *format, ...);
-typedef struct check_type {
-  char arg;
-  int (*func)(va_list);
+/**
+ * struct check_type - structure who take argument and function
+ * @arg: the conversion specifiers
+ * @func: the function to call
+ */
+typedef struct check_type
+{
+	char arg;
+	int (*func)(va_list);
 
 } check_t;
 #endif
