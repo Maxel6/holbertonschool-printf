5ef1dbb8a1b1b4a766cad7042ff8480e49e2746e
Axel Malka <axelmalka@outlook.fr>
2023-03-29 01:06:42 -0700

    fix unknow operator

--- get_func.c
+++ get_func.c
@@ -7,26 +7,34 @@
  * and calls the corresponding function to handle the argument.
  * Return: On success, the number of characters written is returned
  */
+
 int check_type(char c, va_list list)
 {
 	int i = 0;
 	int a = 0;
-
+	int p_flag = 0;
+	int percent = '%';
 	check_t type[] = {
 		{'s', print_s},
 		{'c', print_c},
+		{'d', print_d},
+		{'i', print_d},
 		{'%', print_percent},
-		{0, 0}
-	};
+		{0, 0}};
 
 	while (type[i].arg != 0)
 	{
 		if (c == type[i].arg)
 		{
 			a += type[i].func(list);
+			p_flag = 1;
 		}
 		i++;
 	}
-	return (a);
-}
+	if (p_flag != 1)
+	{
+		printf("toto");
+		write(STDOUT_FILENO, &percent, sizeof(char));
+		write(STDOUT_FILENO, &c, sizeof(char));
+	}
 
